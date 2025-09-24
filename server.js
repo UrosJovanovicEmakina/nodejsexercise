d8ac66982333584c0fa0c9dbd7e4f30672b67640
uros <uros_jovanovic@epam.com>
2025-09-24 23:40:19 +0200

    books added

--- server.js
+++ server.js
@@ -8,13 +8,15 @@ const bodyParser = require('body-parser')
 
 const indexRouter = require('./routes/index')
 const authorRouter = require('./routes/authors')
+const bookRouter = require('./routes/books')
+
 
 app.set('view engine', 'ejs')
 app.set('views', __dirname +'/views')
 app.set('layout', 'layouts/layout')
 app.use(expressLayouts)
 app.use(express.static('public'))
-app.use(bodyParser.urlencoded({limit:'10bm', extended:false}))
+app.use(bodyParser.urlencoded({limit:'10mb', extended:false}))
 
 const mongoose = require('mongoose')
 mongoose.connect(process.env.DATABASE_URL, {})
@@ -25,5 +27,7 @@ db.once('open', () => console.error('Connected to Mongoose'))
 app.use('/', indexRouter)
 
 app.use('/authors', authorRouter)
+
+app.use('/books', bookRouter)
 
 app.listen(process.env.PORT || 3000)
